0c6d72e48bd0e640849a61cc59de587f70c2e182
Rane M Kaseman <raykman@comcast.net>
2018-09-29 17:19:41 -0600

    1719

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 
 import React, { Component } from 'react';
+import Score from "./components/Score";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" /><br/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" /><br/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" /><br/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Score /><h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                    <br/>
+                    {/* <Score /> */}
+                </p>
+                <div>
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                    <img src={logo} className="App-logo-tester" alt="logo" />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
