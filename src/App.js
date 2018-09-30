cb057168807078bbfa836ea410ac4220103c1931
Rane M Kaseman <raykman@comcast.net>
2018-09-29 18:59:01 -0600

    1858

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@
 import React, { Component } from 'react';
 import Score from "./components/Score";
 import logo from './logo.svg';
-import logoRed from './components/Logo/logoRed.svg';
+import logoBlack from './components/Logo/logoBlack.svg';
 import logoBlue from './components/Logo/logoBlue.svg';
+import logoCyan from './components/Logo/logoCyan.svg';
+import logoGreen from './components/Logo/logoGreen.svg';
+import logoGrey from './components/Logo/logoGrey.svg';
+import logoLime from './components/Logo/logoLime.svg';
+import logoOrange from './components/Logo/logoOrange.svg';
+import logoPurple from './components/Logo/logoPurple.svg';
+import logoRed from './components/Logo/logoRed.svg';
+import logoYellow from './components/Logo/logoYellow.svg';
 import './App.css';
 
 class App extends Component {
@@ -22,18 +30,21 @@ class App extends Component {
                     <br/>
                 </p>
                 <div>
-                    <img src={logoRed} className="App-logo-tester" alt="logo" />
+                    <img src={logoBlack} className="App-logo-tester" alt="logo" />
                     <img src={logoBlue} className="App-logo-tester" alt="logo" />
+                    <img src={logoCyan} className="App-logo-tester" alt="logo" />
+                    <img src={logoGreen} className="App-logo-tester" alt="logo" />
+                    <br />
+                    <img src={logoGrey} className="App-logo-tester" alt="logo" />
+                    <img src={logoLime} className="App-logo-tester" alt="logo" />
+                    <img src={logoOrange} className="App-logo-tester" alt="logo" />
+                    <img src={logoPurple} className="App-logo-tester" alt="logo" />
+                    <br />
+                    <img src={logoRed} className="App-logo-tester" alt="logo" />
+                    <img src={logoYellow} className="App-logo-tester" alt="logo" />
                     <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
                     <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
-                    <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" />
-                    <img src={logo} className="App-logo-tester" alt="logo" /><br />
+                    <br />
                     <img src={logo} className="App-logo-tester" alt="logo" />
                     <img src={logo} className="App-logo-tester" alt="logo" />
                     <img src={logo} className="App-logo-tester" alt="logo" />
